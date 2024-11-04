6204480f1cce91bcf84fddd76d4c5e178ae6e25c
Luís Fellype <luisfellypemagalhaes.cat@hotmail.com>
2024-11-04 09:22:30 -0900

    Update biblioteca.h
    alec update

--- biblioteca.h
+++ biblioteca.h
@@ -8,7 +8,7 @@
 typedef struct livro {
     char nome[q];
     char autor[q];
-    int genero; // '1' - Exatas, '2' - Saude, '3' - Humanas
+    int genero; // '1' - Exatas, '2' - Saúde, '3' - Humanas
     int volume;
     int qtd;
     int disponibilidade;
@@ -16,7 +16,11 @@ typedef struct livro {
 
 void cadastrar_livro(livro *l);
 void buscarLivroPorNome(livro livros[], int quantidade, const char *nomeProcurado);
-void listar_livros(const livro livros[], int quantidade); 
+void listar_livros(const livro livros[], int quantidade);
+void atualizar_livro(livro *l);
+void deletar_livro(livro livros[], int *quantidade, int index);
+void salvar_livros(const livro livros[], int quantidade); // Nova função para salvar todos os livros no arquivo
+
 
                                       //FUNÇÕES E PROTÓTIPOS PARA ALUNOS (KELVYN)
 
