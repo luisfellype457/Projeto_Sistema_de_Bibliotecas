e5e881f55d31635e500332b5e45f08f43524fd8d
Luís Fellype <luisfellypemagalhaes.cat@hotmail.com>
2024-11-06 08:37:17 -0900

    Update main.c

--- main.c
+++ main.c
@@ -4,7 +4,12 @@
 int main(){ 
 	int op;
 	do{
-		printf("\tMenu Principal\n(1) - criar biblioteca\n(2) - pesquisar/listar bibliotecas\n\n> ");
+		printf("- - - - - Menu Principal - - - - -\n\n");
+		printf("(1) - Criar biblioteca\n");
+		printf("(2) - Pesquisar/listar bibliotecas\n");
+		printf("(3) - Deletar biblioteca\n");
+		printf("(4) - Atualizar biblioteca\n");
+		printf("(5) - Sair\n\n> ");
 		scanf("%d", &op);
 		fflush(stdin);
 		switch(op){
@@ -14,9 +19,15 @@ int main(){
 			case 2: 
 				mostrar_biblioteca();
 				break;
+			case 3:
+				deletar_biblioteca();
+				break;
+			case 4:
+				update_biblioteca();
+				break;
 			default:
 				break;
 		}
-	} while (op < 3 && op > 0);
+	} while (op < 5 && op > 0);
 	return 0;
 }
