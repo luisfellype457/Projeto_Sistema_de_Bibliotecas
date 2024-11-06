3a80381ce91e4c4c78132bd166394e9c226cad9c
Luís Fellype <luisfellypemagalhaes.cat@hotmail.com>
2024-11-06 09:57:19 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,21 @@
 #include "biblioteca.h"
 
 int main(){ 
-	criar_biblioteca();
+	int op;
+	do{
+		printf("\tMenu Principal\n(1) - criar biblioteca\n(2) - pesquisar/listar bibliotecas\n\n> ");
+		scanf("%d", &op);
+		fflush(stdin);
+		switch(op){
+			case 1:
+				criar_biblioteca();
+				break;
+			case 2: 
+				mostrar_biblioteca();
+				break;
+			default:
+				break;
+		}
+	} while (op < 3 && op > 0);
 	return 0;
 }
