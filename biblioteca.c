c9c74dbaf7ac090666bd97afea762b8c34dee211
Luís Fellype <luisfellypemagalhaes.cat@hotmail.com>
2024-11-06 09:57:05 -0300

    Update biblioteca.c

--- biblioteca.c
+++ biblioteca.c
@@ -19,6 +19,7 @@ void criar_arquivo_biblioteca(){
 		fprintf(p, "Nome: %s\nLocal: %s\nFuncionarios: %d\nAbertura: %s\nFechamento: %s\n\n", library[i].nome, library[i].local, library[i].num_funcionarios, library[i].hora_abertura, library[i].hora_fechamento);
 	}
 	fclose(p);
+	return;
 }
 
 void ler_arquivo(){
@@ -29,13 +30,14 @@ void ler_arquivo(){
 		exit(1);
 	}
 	rewind(p);
-	while(fscanf(p, "%19[^\n]\n %19[^\n]\n %d\n %4[^\n]\n %4[^\n]\n", library[count].nome, library[count].local, &library[count].num_funcionarios, library[count].hora_abertura, library[count].hora_fechamento) == 5){
+	while(fscanf(p, "Nome: %[^\n]\nLocal: %[^\n]\nFuncionarios: %d\nAbertura: %5[^\n]\nFechamento: %5[^\n]\n", library[count].nome, library[count].local, &library[count].num_funcionarios, library[count].hora_abertura, library[count].hora_fechamento) == 5){
 		count++;
 		if (count >= MAX_LIBRARIES)
 			break;
 		fscanf(p, "\n");
 	}
 	fclose(p);
+	return;
 }
 
 void criar_biblioteca(){
@@ -63,14 +65,15 @@ void criar_biblioteca(){
 		count++;
 	} while (!strcmp(confirm, "sim") || !strcmp(confirm, "s"));
 	criar_arquivo_biblioteca();
+	return;
 }
 
 void deletar_biblioteca() {
 	int i, j, found = 0;
-	char nome[20];
+	char nome[30];
 	ler_arquivo();
 	printf("Digite o nome da biblioteca a ser excluída:\n> ");
-	scanf("%19[^\n]", nome);
+	scanf("%29[^\n]", nome);
 	for (i = 0; i < count; i++) {
 		if (!strcmp(library[i].nome, nome)) {
 			found = 1;
@@ -87,4 +90,44 @@ void deletar_biblioteca() {
 	count--;
 	criar_arquivo_biblioteca();
 	printf("Biblioteca '%s' deletada com sucesso.\n", nome);
+	return;
+}
+
+void mostrar_biblioteca(){
+	int i, c, found=0;
+	char nomep[30], confirm[3];
+	ler_arquivo();
+	printf("\n(1) - Pesquisar uma biblioteca\n(2) - Mostrar lista completa\n\n> ");
+	scanf("%d", &c);
+	fflush(stdin);
+	if (c == 1){
+		do{
+			printf("\nDigite o nome da biblioteca:\n> ");
+			scanf("%29[^\n]", nomep);
+			fflush(stdin);
+			for (i=0; i < count; i++){
+				if (!strcmp(library[i].nome, nomep)){
+					found=1;
+					printf("\tBiblioteca encontrada!\n");
+					printf("Nome: %s\n", library[i].nome);
+					printf("Local: %s\n", library[i].local);
+					printf("Funcionarios: %d\n", library[i].num_funcionarios);
+					printf("Horario de abertura: %s\n", library[i].hora_abertura);
+					printf("Horario de fechamento: %s\n\n", library[i].hora_fechamento);
+				}
+			}
+			printf("Deseja repetir a busca? (sim/nao)\n> ");
+			scanf("%3[^\n]", confirm);
+			fflush(stdin);
+		} while(!strcmp(confirm, "sim") || !strcmp(confirm, "s"));
+	} else {
+		for (i=0; i < count; i++){
+			printf("Nome: %s\n", library[i].nome);
+			printf("Local: %s\n", library[i].local);
+			printf("Numero de Funcionarios: %d\n", library[i].num_funcionarios);
+			printf("Horario de abertura: %s\n", library[i].hora_abertura);
+			printf("Horario de fechamento: %s\n\n", library[i].hora_fechamento);
+		}
+	}
+	return;
 }
