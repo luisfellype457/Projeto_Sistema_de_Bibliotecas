6670e5f4c3848e371ddd870e954eb01e5299a724
Emmanuel Souza <148597555+EmmanuelS-S@users.noreply.github.com>
2024-11-10 20:20:28 -0300

    Update computador-main.c

--- computador-main.c
+++ computador-main.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 #include "computador.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	setlocale(LC_ALL,"portuguese");
 	
-	 abrir_arquivo();
+	while(1){
+	
 	adicionar_computador();
-	
+	adicionar_computador();
+	adicionar_computador();
+	listar_computador();
 	remover_computador();
 	listar_computador();
-    alugar_computador();
 	
-	return 0;
+}
+return 0;
 }
