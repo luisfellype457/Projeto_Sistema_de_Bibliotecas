a4410b0d2f48112aaf930559f165e668aa3636bc
Alec-MM <160276745+Alec-MM@users.noreply.github.com>
2024-11-11 14:46:59 -0300

    Update livro.h para utilizar o static file

--- livro.h
+++ livro.h
@@ -1,27 +1,28 @@
 #ifndef LIVRO_H
 #define LIVRO_H
+
 #define q 50
-//livro.h
 
 #include <stdio.h>
 
-extern FILE *arquivoLivros; // Declaração da variável global FILE
-
+// Estrutura para o livro
 typedef struct livro {
     char nome[q];
     char autor[q];
-    int genero; // '1' - Exatas, '2' - Saude, '3' - Humanas
+    int genero; // '1' - Exatas, '2' - Saúde, '3' - Humanas
     int volume;
     int qtd;
     int disponibilidade;
 } livro;
+
+// Funções
 void abrir_arquivo();  // Função que abre o arquivo de livros
 void fechar_arquivo(); // Função que fecha o arquivo
 void criar_livro(livro *l); // CREATE 'C'-> 'CRUD'.
-void buscarLivroPorNome(livro livros[], int quantidade, const char *nomeProcurado); // READ 'R'-> 'CRUD' 
-void listar_livros(const livro livros[], int quantidade); // READ 'R'-> 'CRUD' 
-void atualizar_livro(livro *l); // UPDATE 'U'-> 'CRUD' 
-void deletar_livro(livro livros[], int *quantidade, int index); // DELETE 'D'-> 'CRUD' 
+void buscarLivroPorNome(livro livros[], int quantidade, const char *nomeProcurado); // READ 'R'-> 'CRUD'
+void listar_livros(const livro livros[], int quantidade); // READ 'R'-> 'CRUD'
+void atualizar_livro(livro *l); // UPDATE 'U'-> 'CRUD'
+void deletar_livro(livro livros[], int *quantidade, int index); // DELETE 'D'-> 'CRUD'
 void salvar_livros(const livro livros[], int quantidade); // SALVAR
 
 #endif // LIVRO_H
