61b599e44b26cdfd019e85317bb34875a5c75cb4
Emmanuel Souza <148597555+EmmanuelS-S@users.noreply.github.com>
2024-11-11 14:40:32 -0300

    adicionei menu em computador-main.c

--- computador-main.c
+++ computador-main.c
@@ -8,15 +8,58 @@
 int main(int argc, char *argv[]) {
 	setlocale(LC_ALL,"portuguese");
 	
-	while(1){
+	int op;
 	
-	adicionar_computador();
-	adicionar_computador();
-	adicionar_computador();
-	listar_computador();
-	remover_computador();
-	listar_computador();
+	abrir_arquivo();
 	
-}
+	//como escreve no arquivo?
+	
+	do{
+		
+		puts("Digite a opção desejada:");
+		
+		puts("Adicionar computador(1)");
+		puts("Remover computador(2)");
+		puts("Listar computador(3)");
+		puts("Alugar computador(4)");
+		puts("Devolver computador(5)");
+		puts("Renovar computador(6)");
+		
+		
+		scanf("%d", &op);
+		fflush(stdin);
+		
+		puts("--------------------------------------------");
+		
+		switch(op){
+			
+			case 1:
+				adicionar_computador();
+				break;
+				
+			case 2:
+				remover_computador();
+				break;
+
+			case 3:
+				listar_computador();
+				break;
+			case 4:
+				alugar_computador();
+				break;
+			case 5:
+				devolver_computador();
+				break;
+			case 6:
+				renovar_computador();
+				break;
+			default:
+				puts("\nPor favor digite um valor válido\n");
+		}
+		
+	}while(1);
+	
+	
+	
 return 0;
 }
