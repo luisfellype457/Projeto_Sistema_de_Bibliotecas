0bb6c7be6c1a87b8ac89d5cbfdb0d5c8a24c70d4
Alec-MM <160276745+Alec-MM@users.noreply.github.com>
2024-11-11 15:17:21 -0300

    Update livro.h

--- livro.h
+++ livro.h
@@ -15,10 +15,13 @@ typedef struct livro {
     int disponibilidade;
 } livro;
 
+// Variável externa para manipulação de arquivo
+extern FILE *arquivoLivros;  // Declarando a variável de ponteiro para FILE
+
 // Funções
 void abrir_arquivo();  // Função que abre o arquivo de livros
 void fechar_arquivo(); // Função que fecha o arquivo
-void criar_livro(livro *l); // CREATE 'C'-> 'CRUD'.
+void criar_livro(livro *l); // CREATE 'C'-> 'CRUD'
 void buscarLivroPorNome(livro livros[], int quantidade, const char *nomeProcurado); // READ 'R'-> 'CRUD'
 void listar_livros(const livro livros[], int quantidade); // READ 'R'-> 'CRUD'
 void atualizar_livro(livro *l); // UPDATE 'U'-> 'CRUD'
