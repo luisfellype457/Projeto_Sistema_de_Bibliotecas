e90eda3d85d4aa66ddb20cf4f746c46ac1ae5b88
Alec-MM <160276745+Alec-MM@users.noreply.github.com>
2024-11-11 14:45:36 -0300

    retirado a variavel file nesse arquivo, e ,adicionado um menu em atualizar livros

--- livro.c
+++ livro.c
@@ -3,8 +3,11 @@
 #include <string.h>
 #include "livro.h"
 
-// Variável global do tipo FILE
-FILE *arquivoLivros = NULL;
+
+static FILE *arquivoLivros = NULL; 
+
+int i, encontrado, opcao, index;
+char nomeProcurado[q];
 
 // Função para abrir o arquivo de livros
 void abrir_arquivo() {
@@ -25,11 +28,12 @@ void fechar_arquivo() {
     }
 }
 
+// Função para salvar os livros no arquivo
 void salvar_livros(const livro livros[], int quantidade) {
     abrir_arquivo(); // Abre o arquivo (caso ainda não tenha sido aberto)
 
     // Salva os livros no arquivo
-    for (int i = 0; i < quantidade; i++) {
+    for (i = 0; i < quantidade; i++) {
         fprintf(arquivoLivros, "Livro %d:\n", i + 1);
         fprintf(arquivoLivros, "  Nome: %s\n", livros[i].nome);
         fprintf(arquivoLivros, "  Autor: %s\n", livros[i].autor);
@@ -38,10 +42,9 @@ void salvar_livros(const livro livros[], int quantidade) {
         fprintf(arquivoLivros, "  Quantidade: %d\n", livros[i].qtd);
         fprintf(arquivoLivros, "  Disponibilidade: %s\n\n", livros[i].disponibilidade ? "Disponivel" : "Indisponivel");
     }
-
-    // Não precisa fechar o arquivo aqui, pois a variável global será reutilizada em outras funções
 }
 
+// Função para criar um novo livro
 void criar_livro(livro *l) {
     printf("Digite o nome do livro: ");
     fgets(l->nome, q, stdin);
@@ -64,11 +67,12 @@ void criar_livro(livro *l) {
     printf("Livro criado com sucesso!\n");
 }
 
+// Função para buscar um livro pelo nome
 void buscarLivroPorNome(livro livros[], int quantidade, const char *nomeProcurado) {
-    int encontrado = 0;
+    encontrado = 0;
 
     printf("Resultados da busca por '%s':\n", nomeProcurado);
-    for (int i = 0; i < quantidade; i++) {
+    for (i = 0; i < quantidade; i++) {
         if (strstr(livros[i].nome, nomeProcurado) != NULL) {
             printf("Nome: %s\n", livros[i].nome);
             printf("Autor: %s\n", livros[i].autor);
@@ -85,6 +89,7 @@ void buscarLivroPorNome(livro livros[], int quantidade, const char *nomeProcurad
     }
 }
 
+// Função para listar todos os livros cadastrados
 void listar_livros(const livro livros[], int quantidade) {
     if (quantidade == 0) {
         printf("Nenhum livro cadastrado.\n");
@@ -92,7 +97,7 @@ void listar_livros(const livro livros[], int quantidade) {
     }
 
     printf("Lista de Livros:\n");
-    for (int i = 0; i < quantidade; i++) {
+    for (i = 0; i < quantidade; i++) {
         printf("Livro %d:\n", i + 1);
         printf("  Nome: %s\n", livros[i].nome);
         printf("  Autor: %s\n", livros[i].autor);
@@ -103,34 +108,71 @@ void listar_livros(const livro livros[], int quantidade) {
     }
 }
 
+// Função para atualizar informações de um livro
 void atualizar_livro(livro *l) {
-    printf("Atualizando informacoes do livro:\n");
+    do {
+        printf("\nO que deseja atualizar?\n");
+        printf("1. Nome\n");
+        printf("2. Autor\n");
+        printf("3. Gênero\n");
+        printf("4. Volume\n");
+        printf("5. Quantidade\n");
+        printf("6. Disponibilidade\n");
+        printf("7. Atualizar todos os campos\n");
+        printf("8. Voltar\n");
+        printf("Escolha uma opcao: ");
+        scanf("%d", &opcao);
+        getchar(); // Limpa o buffer do teclado
 
-    printf("Novo nome (atual: %s): ", l->nome);
-    fgets(l->nome, q, stdin);
-    l->nome[strcspn(l->nome, "\n")] = 0;
-
-    printf("Novo autor (atual: %s): ", l->autor);
-    fgets(l->autor, q, stdin);
-    l->autor[strcspn(l->autor, "\n")] = 0;
-
-    printf("Novo gênero (atual: %d): ", l->genero);
-    scanf("%d", &l->genero);
-
-    printf("Novo volume (atual: %d): ", l->volume);
-    scanf("%d", &l->volume);
-
-    printf("Nova quantidade (atual: %d): ", l->qtd);
-    scanf("%d", &l->qtd);
-
-    printf("Livro atualizado com sucesso!\n");
+        switch (opcao) {
+            case 1:
+                printf("Novo nome (atual: %s): ", l->nome);
+                fgets(l->nome, q, stdin);
+                l->nome[strcspn(l->nome, "\n")] = 0;
+                break;
+            case 2:
+                printf("Novo autor (atual: %s): ", l->autor);
+                fgets(l->autor, q, stdin);
+                l->autor[strcspn(l->autor, "\n")] = 0;
+                break;
+            case 3:
+                printf("Novo genero (atual: %d): ", l->genero);
+                scanf("%d", &l->genero);
+                getchar();
+                break;
+            case 4:
+                printf("Novo volume (atual: %d): ", l->volume);
+                scanf("%d", &l->volume);
+                getchar();
+                break;
+            case 5:
+                printf("Nova quantidade (atual: %d): ", l->qtd);
+                scanf("%d", &l->qtd);
+                getchar();
+                break;
+            case 6:
+                printf("Nova disponibilidade (atual: %s): ", l->disponibilidade ? "Disponivel" : "Indisponivel");
+                printf("Digite 1 para disponivel ou 0 para indisponivel: ");
+                scanf("%d", &l->disponibilidade);
+                break;
+            case 7:
+                criar_livro(l);
+                break;
+            case 8:
+                printf("Voltando...\n");
+                break;
+            default:
+                printf("Opcao invalida!\n");
+                break;
+        }
+    } while (opcao != 8);
 }
 
+// Função para deletar um livro da lista
 void deletar_livro(livro livros[], int *quantidade, int index) {
-    for (int i = index; i < (*quantidade) - 1; i++) {
+    for (i = index; i < (*quantidade) - 1; i++) {
         livros[i] = livros[i + 1];
     }
     (*quantidade)--; // Diminui a quantidade de livros
     printf("Livro deletado com sucesso!\n");
 }
-
