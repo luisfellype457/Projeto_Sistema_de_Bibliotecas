2177e1b9669f72abf82f0cdaa8c9674e927019a6
Luís Fellype <luisfellypemagalhaes.cat@hotmail.com>
2024-11-15 18:46:40 -0300

    Update catalogo.c

--- catalogo.c
+++ catalogo.c
@@ -1,160 +1,243 @@
-#include "catalogo.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include "computador.h"
+#define MAX 30
 
-Cadastro catalogo[Numero_Maximo_de_Livros];
-int totalLivros = 0;
+int quantidade_computador = 0;
+Computador computador[MAX];
 
-char nome[Comprimento_maximo];
-char categoria[Tamanho_Maximo_da_Categoria];
-int emprestado, i;
+void abrir_arquivo() {
+	int i;
+	
+    f = fopen("Computadores.txt", "w");
+    if (f == NULL) {
+        printf("Erro na abertura do arquivo!\n");
+        exit(1);
+    }
+    for ( i = 0; i < quantidade_computador; i++) {
+    	//tirei a parte que nao mostrava a situacao == 3;
+            fprintf(f, "Computador.id:%d Computador.situacao:%d\n", computador[i].id, computador[i].situacao);
+    }
+    fclose(f);
+}
 
-void menu_catalogo(){
-	int opcao;
+void carregar_arquivo() {
+    FILE *f = fopen("Computadores.txt", "r");
+    if (f == NULL) {
+        printf("Ainda nao ha dados de computadores. O arquivo sera criado quando salvar novos dados.\n");
+        return;
+    }
 
-    CarregarCatalogo();
+    while (fscanf(f, "Computador.id:%d Computador.situacao:%d", 
+                  &computador[quantidade_computador].id, 
+                  &computador[quantidade_computador].situacao) == 2) {
+        quantidade_computador++;
+    }
+
+    fclose(f);
+}
+
+void adicionar_computador() {
+    int i, teste = 0;
+    if (quantidade_computador == MAX) {
+        puts("Quantidade maxima de computadores atingida");
+        return;
+    }
+
+    Computador novo_pc;
+    puts("Digite o id do computador que deseja adicionar");
+    scanf("%d", &novo_pc.id);
+
+    for (i = 0; i < quantidade_computador; i++) {
+        if (novo_pc.id == computador[i].id) {
+            if (computador[i].situacao == 2) {  // Caso o computador tenha sido removido
+                computador[i].situacao = 1;  // Reativa o computador removido
+                puts("Computador reativado com sucesso!");
+                return;
+            }
+            puts("Ja existe um computador com esse ID");
+            return;
+        }
+    }
+
+    novo_pc.situacao = 1;  // DisponÃ­vel
+    computador[quantidade_computador++] = novo_pc;
+    abrir_arquivo();
+}
+
+void remover_computador() {
+    int i, id_remover;
+    int computador_encontrado = 0;
 
     do {
-    	printf("\n- - - - - [Menu Catalogo] - - - - -\n\n");
-        printf("\n1. Adicionar Livro\n2. Listar Livros\n3. Remover Livro\n4. Atualizar Emprestimo\n5. Verificar Emprestimo\n6. Salvar e Sair\nEscolha uma opcao: ");
-        scanf("%d", &opcao);
-        getchar();
+        puts("Digite o id do computador que deseja remover");
+        scanf("%d", &id_remover);
+    } while (id_remover <= 0 || id_remover > MAX);
 
-        switch (opcao) {
-            case 1:
-                printf("Nome do Livro: ");
-                fgets(nome, Comprimento_maximo, stdin);
-                nome[strcspn(nome, "\n")] = '\0';
-
-                printf("Categoria do Livro: ");
-                fgets(categoria, Tamanho_Maximo_da_Categoria, stdin);
-                categoria[strcspn(categoria, "\n")] = '\0';
-
-                AdicionarLivro(nome, categoria);
-                break;
-
-            case 2:
-                ListarLivros();
-                break;
-
-            case 3:
-                printf("Nome do Livro a remover: ");
-                fgets(nome, Comprimento_maximo, stdin);
-                nome[strcspn(nome, "\n")] = '\0';
-                RemoverLivro(nome);
-                break;
-
-            case 4:
-                printf("Nome do Livro para atualizar o emprestimo: ");
-                fgets(nome, Comprimento_maximo, stdin);
-                nome[strcspn(nome, "\n")] = '\0';
-
-                printf("Emprestado? (1 para Sim, 0 para Nao): ");
-                scanf("%d", &emprestado);
-
-                AtualizarEmprestimo(nome, emprestado);
-                break;
-
-            case 5:
-                printf("Nome do Livro para verificar emprestimo: ");
-                fgets(nome, Comprimento_maximo, stdin);
-                nome[strcspn(nome, "\n")] = '\0';
-
-                emprestado = verificarEmprestimo(nome);
-                if (emprestado == -1) {
-                    printf("Livro nao encontrado.\n");
-                } else {
-                    printf("O livro esta %semprestado.\n", emprestado ? "" : "nao ");
-                }
-                break;
-
-            case 6:
-                SalvarCatalogo();
-                printf("Saindo...\n");
-                break;
-
-            default:
-                printf("Opção invalida. Tente novamente.\n");
+    for (i = 0; i < quantidade_computador; i++) {
+        if (computador[i].id == id_remover) {
+            computador_encontrado = 1;
+            computador[i].situacao = 2;  // Marca o computador como removido
+            printf("Computador com ID %d removido com sucesso.\n", id_remover);
+            break;
         }
-    } while (opcao != 6);
+    }
+
+    if (!computador_encontrado) {
+        puts("Computador nao encontrado com o ID fornecido.");
+    }
+    
+    abrir_arquivo();
 }
 
-void AdicionarLivro(char *nome, char *categoria) {
-    if (totalLivros < Numero_Maximo_de_Livros) {
-        strncpy(catalogo[totalLivros].nome, nome, Comprimento_maximo);
-        strncpy(catalogo[totalLivros].categoria, categoria, Tamanho_Maximo_da_Categoria);
-        catalogo[totalLivros].emprestado = 0;
-        totalLivros++;
-    } else {
-        printf("Catalogo cheio! Nao e possivel adicionar mais livros.\n");
+void listar_computador() {
+    int i;
+
+    if (!quantidade_computador) {
+        puts("Nenhum computador cadastrado");
+        return;
+    }
+
+    for (i = 0; i < quantidade_computador; i++) {
+        if (computador[i].situacao == 2) {
+            continue;  // Ignora computadores removidos
+        }
+
+        printf("\nComputador %d\n", i + 1);
+        printf("ID: %d\n", computador[i].id);
+        if (computador[i].situacao == 1) {
+            puts("Disponivel");
+        } else {
+            puts("Indisponivel");
+        }
+        puts("-----------------------------");
     }
 }
 
-void ListarLivros() {
-    for (i = 0; i < totalLivros; i++) {
-        printf("Nome: %s\nCategoria: %s\nEmprestado: %s\n\n", 
-               catalogo[i].nome, catalogo[i].categoria, 
-               catalogo[i].emprestado ? "Sim" : "NÃ£o");
-    }
-}
+void alugar_computador() {
+    int i, id_aluguel, situacao = 0;
 
-void RemoverLivro(char *nome) {
-	int j;
-    for (i = 0; i < totalLivros; i++) {
-        if (strcmp(catalogo[i].nome, nome) == 0) {
-            for (j = i; j < totalLivros - 1; j++) {
-                catalogo[j] = catalogo[j + 1];
+    puts("\nComputadores disponiveis:\n");
+    listar_computador();
+
+    puts("Digite qual o id do computador deseja alugar?");
+    scanf("%d", &id_aluguel);
+
+    for (i = 0; i < quantidade_computador; i++) {
+        if (computador[i].id == id_aluguel) {
+            situacao = 1;
+
+            if (computador[i].situacao == 1) {
+                computador[i].situacao = 0;  // Marca como indisponÃ­vel
+                printf("Computador %d alugado com sucesso!\n", id_aluguel);
+            } else {
+                puts("Esse computador esta indisponivel para aluguel.");
             }
-            totalLivros--;
-            printf("Livro removido com sucesso.\n");
-            return;
+            break;
         }
     }
-    printf("Livro nao encontrado.\n");
+
+    if (!situacao) {
+        puts("ID invalido. Computador nao encontrado.");
+    }
 }
 
-void AtualizarEmprestimo(char *nome, int emprestado) {
-    for (i = 0; i < totalLivros; i++) {
-        if (strcmp(catalogo[i].nome, nome) == 0) {
-            catalogo[i].emprestado = emprestado;
-            printf("Status de emprestimo atualizado.\n");
-            return;
+void devolver_computador() {
+    int id_devolucao, i, existe = 0;
+
+    puts("\nDigite qual computador deseja devolver:");
+    scanf("%d", &id_devolucao);
+
+    for (i = 0; i < quantidade_computador; i++) {
+        if (id_devolucao == computador[i].id) {
+            existe = 1;
+            if (computador[i].situacao == 1) {
+                puts("Esse computador ainda nao foi alugado.");
+            } else {
+                computador[i].situacao = 1;  // Marca como disponÃ­vel
+                puts("Computador devolvido com sucesso.");
+            }
+            break;
         }
     }
-    printf("Livro nao encontrado.\n");
+
+    if (!existe) {
+        puts("O computador inserido nao existe.");
+    }
 }
 
-int verificarEmprestimo(char *nome) {
-    for (i = 0; i < totalLivros; i++) {
-        if (strcmp(catalogo[i].nome, nome) == 0) {
-            return catalogo[i].emprestado;
+void renovar_computador() {
+    int i, renovar = 0;
+
+    puts("Qual computador deseja renovar?");
+    scanf("%d", &renovar);
+
+    for (i = 0; i < quantidade_computador; i++) {
+        if (computador[i].situacao == 0) {  // Se o computador estiver alugado
+            computador[i].situacao = 0;  // Apenas confirma o status "alugado"
+            printf("Computador %d renovado com sucesso!\n", computador[i].id);
+            renovar++;
         }
     }
-    return -1;
+
+    if (!renovar) {
+        puts("Nao ha computadores alugados com esse ID.");
+    }
 }
 
-void SalvarCatalogo() {
-    FILE *file = fopen(ARQUIVO_LIVROS, "w");
-    if (file == NULL) {
-        printf("Erro ao abrir o arquivo para salvar.\n");
-        return;
-    }
-    for (i = 0; i < totalLivros; i++) {
-        fprintf(file, "%s;%s;%d\n", catalogo[i].nome, catalogo[i].categoria, catalogo[i].emprestado);
-    }
-    fclose(file);
-    printf("Catalogo salvo com sucesso.\n");
-}
+void menu_computadores(){
+	int op;
+	
+	carregar_arquivo();
+	
+	
+	do{
+		printf("\n- - - - - [Menu Computadores] - - - - -\n\n");
+		puts("Digite a opcao desejada:\n");
+		
+		puts("(1) - Adicionar computador");
+		puts("(2) - Remover computador");
+		puts("(3) - Listar computador");
+		puts("(4) - Alugar computador");
+		puts("(5) - Devolver computador");
+		puts("(6) - Renovar computador");
+		puts("(7) - Sair");
+		printf("\n> ");
+		
+		scanf("%d", &op);
+		fflush(stdin);
+		
+		puts("--------------------------------------------");
+		
+		switch(op){
+			
+			case 1:
+				adicionar_computador();
+				break;
+				
+			case 2:
+				remover_computador();
+				break;
 
-void CarregarCatalogo() {
-    FILE *file = fopen(ARQUIVO_LIVROS, "r");
-    if (file == NULL) {
-        printf("Arquivo de catalogo nao encontrado.\n");
-        return;
-    }
-    totalLivros = 0;
-    while (fscanf(file, "%[^;];%[^;];%d\n", catalogo[totalLivros].nome, catalogo[totalLivros].categoria, 
-                  &catalogo[totalLivros].emprestado) == 3) {
-        totalLivros++;
-    }
-    fclose(file);
-    printf("Catalogo carregado com sucesso.\n");
+			case 3:
+				listar_computador();
+				break;
+			case 4:
+				alugar_computador();
+				break;
+			case 5:
+				devolver_computador();
+				break;
+			case 6:
+				renovar_computador();
+				break;
+			case 7:
+				puts("\nSaindo...\n");
+				break;
+			default:
+				puts("\nPor favor digite um valor valido\n");
+		}
+		
+	}while(op != 7);
 }
