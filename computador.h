5618d8def2a6ff8fef022b85edb7616b5cf9f5db
Luís Fellype <luisfellypemagalhaes.cat@hotmail.com>
2024-11-15 13:07:17 -0300

    Create computador.h

---
+++ computador.h
@@ -0,0 +1,23 @@
+#ifndef COMPUTADOR_H
+#define COMPUTADOR_H
+#define MAX 30
+
+typedef struct computador{
+	int id;
+	int situacao;
+}Computador;
+
+FILE *f;
+
+
+void abrir_arquivo();
+void carregar_arquivo();
+void adicionar_computador();
+void remover_computador();
+void listar_computador();
+void alugar_computador();
+void devolver_computador();
+void renovar_computador();
+void menu_computadores();
+
+#endif
